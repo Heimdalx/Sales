cf69e4e6e0c994b0649e30f7f210fe78e6c611ec
David Vasquez Rios <davidvasquez235678@correo.itm.edu.co>
2023-02-21 11:49:28 -0500

    Complete countries API Controller

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -26,10 +26,43 @@ namespace Sales.API.Controllers
             return Ok(country);
         }
 
+
         [HttpGet]
         public async Task<ActionResult> GetAsync()
         {
-            return Ok( await _context.Countries.ToListAsync());
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(country => country.Id == id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(country => country.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
 
     }
